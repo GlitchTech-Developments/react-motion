95eb18112d8a843551e14e8fd92647f81ad00f5b
Jesse Koldewijn <jesse@iungo.nl>
2023-05-26 13:43:18 +0200

    init: codebase creation

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { useMotion } from "./motion";
---
+++ src/motion.ts
@@ -0,0 +1,5 @@
+"use client";
+
+import { ReactElement, CSSProperties } from "react";
+
+export const useMotion = (element: ReactElement, options: CSSProperties) => {};
