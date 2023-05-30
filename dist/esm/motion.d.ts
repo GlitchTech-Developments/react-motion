0092f71cf21fd8b6258c2beddb947d3ccccf5e62
Jesse Koldewijn <jesse@iungo.nl>
2023-05-30 14:29:22 +0200

    cleanup: removed esm output flow for now. needs more research for working setup

--- dist/esm/motion.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { type AnimationOptionsWithOverrides, type ElementOrSelector, type MotionKeyframesDefinition } from "motion";
-export declare const useMotionAnimate: (element: ElementOrSelector, keyframeStyle: MotionKeyframesDefinition, animationConfig?: AnimationOptionsWithOverrides) => void;
-export declare const Motion: ({ children, animate, transition, }: {
-    children: React.ReactNode;
-    animate: MotionKeyframesDefinition;
-    transition?: AnimationOptionsWithOverrides | undefined;
-}) => React.JSX.Element;
